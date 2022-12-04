df3cf52257e3710bfc3764e58e727adca3a3093b
Diniboy1123 <Diniboy1123@users.noreply.github.com>
2022-12-04 23:50:05 +0100

    davinci: Kenvyra bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_davinci.mk
+    $(LOCAL_DIR)/kenvyra_davinci.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_davinci-user \
-    lineage_davinci-userdebug \
-    lineage_davinci-eng
+    kenvyra_davinci-user \
+    kenvyra_davinci-userdebug \
+    kenvyra_davinci-eng
